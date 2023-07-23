b275266015a2190c098a2a7b82df04148c008a7a
Wddddddddddddddddd <107760983+Wddddddddddddddddd@users.noreply.github.com>
2023-07-23 14:28:35 +0800

    search

--- search.py
+++ search.py
@@ -38,13 +38,26 @@ from point import Point
 #     if if_in(up):
 #         q.append(right)
 
-food = Point(2, 5)#(行, 列)
-head = Point(20)
+# food = Point(2, 5) # (行, 列)
+# head = Point(20,20)
+# snakes = [Point(20, 21), Point(20, 22), Point(19, 22), Point(18, 22),
+#           Point(18, 21), Point(18, 20), Point(18, 19), Point(18, 18)]
+
+food = (2, 5) # (列， 行)
+head = (20,20)
+snakes = [(21, 20), (22, 20), (22, 19), (22, 18),
+          (21, 18), (20, 18), (19, 18), (18, 18)]
 
 
+def search(point, des):
+    x, y = point[0], point[1]
+    up, down, left, right = (x, y-1), (x, y+1), (x-1, y), (x+1, y)
+    if point != des:
+        return search(up, des), search(left,des), search(down, des), search(right,des)
+    if point == des:
+        return point
 
-
-
+search(head, food)
 
 
 
